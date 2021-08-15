55601e2f45096043cae70c34caaa3e93cbd3e475
mdcodegunes <64346492+mdcodegunes@users.noreply.github.com>
2021-08-15 17:38:54 +0300

    Add files via upload

---
+++ day01.py
@@ -0,0 +1,41 @@
+print("Day 1- Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+print("\nHello world!\nHello world!\nHello world!\n")
+
+print("Hello" + " Angela")
+print("Hello" + "  " + "Angelan\n")
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the " + '"+"' + " sign.")
+#or
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backlash and n.\n"))
+
+#print("Hello " + input(" What is your name?") + "!")
+#x=input("What is your name?")
+
+#print(len(x))
+
+#or
+
+#print(len(input("What is your name?")))
+
+#
+a = input("a:")
+b = input("b:")
+c = a
+a = b
+b = c
+
+print("a=" + a)
+print("b=" + b)
+
+print("Welcome to the Band Name Generator.")
+a = input("What's name of the city you grew up in?")
+print(a)
+print(b)
+b = input("What's your pet's name?")
+print("Your band name could be" + a + b)
